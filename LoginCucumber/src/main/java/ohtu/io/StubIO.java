f4bd63cfe7566d8602cf30a596c40d9f86d152db
kapistelijaKrisu <krisuppi@yahoo.com>
2017-11-15 15:46:08 +0200

    ops

--- LoginCucumber/src/main/java/ohtu/io/StubIO.java
+++ LoginCucumber/src/main/java/ohtu/io/StubIO.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Component;
 
-@Component
 public class StubIO implements IO {
 
     private List<String> lines;
